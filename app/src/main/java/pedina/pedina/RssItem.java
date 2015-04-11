b7b261122bcc7fc05b71b8b651a2733ac58db37e
Invisible89 <alessandro.datri@live.it>
2015-04-11 16:41:59 +0200

    Rss e nuovi loghi

---
+++ app/src/main/java/pedina/pedina/RssItem.java
@@ -0,0 +1,18 @@
+package pedina.pedina;
+
+public class RssItem {
+
+    private final String title;
+    private final String link;
+
+    public RssItem(String title, String link){
+
+        this.title = title;
+        this.link = link;
+    }
+
+    public String getTitle() { return title;}
+
+    public String getLink() { return link;}
+    
+}
